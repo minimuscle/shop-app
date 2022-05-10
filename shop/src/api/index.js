30b06d9113a7d766c89fced592b1d86fe3048afb
minimuscle <joshthiele@live.com.au>
2022-05-10 16:07:33 +1000

    basic shop

--- shop/src/api/index.js
+++ shop/src/api/index.js
@@ -3,7 +3,9 @@ const sqlite3 = require('sqlite3').verbose()
 const express = require('express')
 const app = express()
 const port = 8080
+const cors = require('cors');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- shop/src/views/Shop.jsx
+++ shop/src/views/Shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Container,
   Card,
@@ -7,17 +7,37 @@ import {
   Button,
   CardActions,
 } from '@mui/material'
+import axios from 'axios'
 
 function Shop() {
+  const [products, setProducts] = useState()
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: 'http://localhost:8080/products',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      setProducts(res.data)
+    })
+  })
+
+  //Map the array of products to cards
+  async function productCard() {
+    await console.log(products)
+  }
+
   return (
     <Container>
       <h1>Shadowvale Shop</h1>
 
       {
-        {
-          /** // TODO: This code here should be iterated over */
-        }
+        /** // TODO: This code here should be iterated over */
+        productCard()
       }
+
       <Card sx={{ maxWidth: 345 }}>
         <CardContent>
           <Typography gutterBottom variant='h5' component='div'>

95846cf56d6b8ae50218d62e0501e0d37a07f4c8
Minimuscle <joshthiele@live.com.au>
2022-06-21 10:51:43 +1000

    add motion and card info design change

--- shop/src/views/shop/CardInfo.jsx
+++ shop/src/views/shop/CardInfo.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Container } from '@mui/material'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Container, Grid } from "@mui/material";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import {
   Card,
   CardContent,
@@ -10,82 +10,94 @@ import {
   CardActions,
   CardHeader,
   CardMedia,
-} from '@mui/material'
+} from "@mui/material";
 
 function CardInfo() {
-  const { productId } = useParams()
-  const [productInfo, setProductInfo] = useState(['Database Loading'])
+  const { productId } = useParams();
+  const [productInfo, setProductInfo] = useState(["Database Loading"]);
 
   useEffect(() => {
     async function getProducts() {
       const response = await axios({
-        method: 'GET',
+        method: "GET",
         url: `http://localhost:8080/product/${productId}`,
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      })
-      setProductInfo([response.data])
+      });
+      setProductInfo([response.data]);
     }
-    getProducts()
-  }, [productId])
+    getProducts();
+  }, [productId]);
 
   return (
     <Container>
       {productInfo.map((product) => {
         return (
-          <div>
-            <h1>{product.name}</h1>
-            <Card sx={{ maxWidth: 1000 }} key={product} className='productInfo'>
-              <CardHeader
-                title={product.name}
-                subheader={
-                  product.parent == null ? (
-                    <div>{product.category}</div>
+          <Grid container key={product} className='productInfo'>
+            <Grid item xs={4}>
+              <Typography variant='h3'>{product.name}</Typography>
+            </Grid>
+            <Grid item xs={8} className='cardInfoPrice'>
+              <Typography variant='h4'>
+                {product.sale_price == null ? (
+                  <div>${product.price}</div>
+                ) : (
+                  <div className='salePrice'>
+                    ${product.sale_price}&#160;
+                    <span>${product.price}</span>
+                  </div>
+                )}
+              </Typography>
+            </Grid>
+            <CardHeader
+              title={product.name}
+              subheader={
+                product.parent == null ? (
+                  <div>{product.category}</div>
+                ) : (
+                  <div>
+                    {product.parent}&#160;&#8226;&#160;
+                    {product.category}
+                  </div>
+                )
+              }
+              action={
+                <Typography variant='h4'>
+                  {product.sale_price == null ? (
+                    <div>${product.price}</div>
                   ) : (
-                    <div>
-                      {product.parent}&#160;&#8226;&#160;
-                      {product.category}
+                    <div className='salePrice'>
+                      ${product.sale_price}&#160;
+                      <span>${product.price}</span>
                     </div>
-                  )
-                }
-                action={
-                  <Typography variant='h4'>
-                    {product.sale_price == null ? (
-                      <div>${product.price}</div>
-                    ) : (
-                      <div className='salePrice'>
-                        ${product.sale_price}&#160;
-                        <span>${product.price}</span>
-                      </div>
-                    )}
-                  </Typography>
-                }
-              />
+                  )}
+                </Typography>
+              }
+            />
 
-              <CardMedia
-                component='img'
-                sx={{ maxWidth: 1000, maxHeight: 600 }}
-                image={
-                  product.image == null
-                    ? 'https://via.placeholder.com/1000x600?text=1000x600+No+Image'
-                    : 'https://via.placeholder.com/400x300?text=Loading+Image'
-                }
-                alt='Placeholder'
-              />
-              <CardContent>
-                <Typography variant='body2'>{product.description}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button size='small'>Share</Button>
-                <Button size='small'>Learn More</Button>
-              </CardActions>
-            </Card>
-          </div>
-        )
+            <CardMedia
+              component='img'
+              sx={{ maxWidth: 1000, maxHeight: 600 }}
+              image={
+                product.image == null
+                  ? "https://via.placeholder.com/1000x600?text=1000x600+No+Image"
+                  : "https://via.placeholder.com/400x300?text=Loading+Image"
+              }
+              alt='Placeholder'
+            />
+            <CardContent>
+              <Typography variant='body2'>{product.description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size='small'>Share</Button>
+              <Button size='small'>Learn More</Button>
+            </CardActions>
+          </Grid>
+        );
       })}
     </Container>
-  )
+  );
 }
 
-export default CardInfo
+export default CardInfo;

5bb397b4b47c4a83e4ace2349e2b238d5b3eabad
Minimuscle <joshthiele@live.com.au>
2022-06-24 08:25:44 +1000

    Shop display

--- shop/src/views/shop/ShopCard.jsx
+++ shop/src/views/shop/ShopCard.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Card,
   CardContent,
@@ -8,14 +8,16 @@ import {
   CardActions,
   CardHeader,
   CardMedia,
-} from "@mui/material"
-import SendIcon from '@mui/icons-material/Send'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import axios from "axios"
-import "../../App.css"
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import axios from "axios";
+import "../../App.css";
 
 function ShopCard() {
   const [products, setProducts] = useState(["Database Loading"]);
+  // TODO: This will eventually be set by the user
+  const cardNum = 3;
 
   useEffect(() => {
     async function getProducts() {
@@ -29,16 +31,32 @@ function ShopCard() {
       setProducts(response.data);
     }
     getProducts();
+
+    //This sets the side of the images and the text based on the number of cards, this will have a max side and min size.
   }, []);
 
   return (
     <div>
       {products.map((product, key) => {
         return (
-          <Card sx={{ maxWidth: 400, height: 550 }} key={key} className='card'>
+          <Card
+            sx={{ maxWidth: 1090 / cardNum, height: 550 }}
+            key={key}
+            className='card'
+          >
             <Link to={`/shop/product/${product.product_id}`}>
               <CardHeader
-                title={product.name}
+                sx={{
+                  display: "flex",
+                  overflow: "hidden",
+                  lineClamp: "3",
+                  "& .MuiCardHeader-content": {
+                    overflow: "hidden",
+                  },
+                  height: "75px",
+                  verticalAlign: "text-top",
+                }}
+                title={<Typography variant='h5'>{product.name}</Typography>}
                 subheader={
                   product.parent == null ? (
                     <div>{product.category}</div>
@@ -75,10 +93,14 @@ function ShopCard() {
               />
             </Link>
             <CardContent>
-              <Typography variant='body2' className="desc">{product.description}</Typography>
+              <Typography variant='body2' className='desc'>
+                {product.description}
+              </Typography>
             </CardContent>
-            <CardActions className="buttons">
-              <Button size='small' endIcon={<ShoppingCartIcon />}>Add to Cart</Button>
+            <CardActions className='buttons'>
+              <Button size='small' endIcon={<ShoppingCartIcon />}>
+                Add to Cart
+              </Button>
               <Button variant='contained' endIcon={<SendIcon />}>
                 Buy Now
               </Button>

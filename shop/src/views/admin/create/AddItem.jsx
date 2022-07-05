4be4b6e15c20afd1d5c6ef657453b3e2757ebc7a
minimuscle <joshthiele@live.com.au>
2022-07-05 11:03:59 +1000

    Added form

--- shop/src/views/admin/create/AddItem.jsx
+++ shop/src/views/admin/create/AddItem.jsx
@@ -1,11 +1,144 @@
 import React from 'react'
-import { Container } from '@mui/material'
-import Select from 'react-select'
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  Grid,
+  Typography,
+  Container,
+} from '@mui/material'
+import { makeStyles } from '@mui/styles'
+import { Formik, Form } from 'formik'
+import CreatableSelect from 'react-select/creatable'
 
 export default function AddItem() {
+  function handleChange() {
+    console.log('Changed')
+  }
+
+  //TODO: Replace this with a table in the database of all the tags ever made
+  const colourOptions = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },
+  ]
+
   return (
-    <Container className="content">
-      <h1>Add New Item</h1>
+    <Container component="main" className="content">
+      <CssBaseline />
+      <div className="paper">
+        <Typography component="h1" variant="h5">
+          Add New Product
+        </Typography>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+          }}
+          onSubmit={(values) => {
+            console.log(values)
+          }}
+        >
+          {({ errors, handleChange, touched }) => (
+            <Form className="form">
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    error={errors.firstName && touched.firstName}
+                    autoComplete="fname"
+                    name="firstName"
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    helperText={
+                      errors.firstName && touched.firstName
+                        ? errors.firstName
+                        : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    error={errors.lastName && touched.lastName}
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="lname"
+                    helperText={
+                      errors.lastName && touched.lastName
+                        ? errors.lastName
+                        : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    error={errors.email && touched.email}
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    helperText={
+                      errors.email && touched.email ? errors.email : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    error={errors.password && touched.password}
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    helperText={
+                      errors.password && touched.password
+                        ? errors.password
+                        : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <label htmlFor="tags">Tags:</label>
+                  <CreatableSelect isMulti options={colourOptions} />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className="submitBtn"
+                  >
+                    Add Item
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </Container>
   )
 }

c39614872ef20712bb8e00a4396e83361ed8798d
minimuscle <joshthiele@live.com.au>
2022-07-05 16:51:33 +1000

    responsive header implemented

--- shop/src/views/components/Header.jsx
+++ shop/src/views/components/Header.jsx
@@ -1,29 +1,86 @@
 /* eslint-disable react/prop-types */
 import React, { useContext } from 'react'
-import { Container } from '@mui/material'
+import {
+  Container,
+  Drawer,
+  Grid,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 import { Link } from 'react-router-dom'
 import { C } from '../context/CartContext'
 
 function Header() {
   const { cart } = useContext(C)
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <ul>
+      <li>
+        <Link to="/cart">Cart ({cart.length})</Link>
+      </li>
+      <li>
+        <Link to="/shop">Shop</Link>
+      </li>
+      <li>
+        <Link to="/new">Add Product</Link>
+      </li>
+    </ul>
+  )
 
   return (
     <div className="header">
       <Container>
-        <Link to="/shop">
-          <h1>Shadowvale Crafts</h1>
-        </Link>
-        <ul>
-          <li>
-            <Link to="/cart">Cart ({cart.length})</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link to="/new">Add Product</Link>
-          </li>
-        </ul>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            <Link to="/shop">
+              <h1>Shadowvale Crafts</h1>
+            </Link>
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Grid
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'inline-block' },
+              float: 'right',
+            }}
+            open
+          >
+            {drawer}
+          </Grid>
+        </Toolbar>
+        <Drawer
+          variant="temporary"
+          anchor="right"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: '200px',
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
       </Container>
     </div>
   )

bcc49dbae4a1a2f0e64540293537330a6fad85b4
minimuscle <joshthiele@live.com.au>
2022-07-07 14:46:02 +1000

    fixed posting

--- shop/gource.sh
+++ shop/gource.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gource gource.txt -f --auto-skip-seconds 1 --date-format "%A, %d %B, %Y" --file-idle-time 0 --key --bloom-intensity 0.35 --title "Shop Application Development" \
+gource gource.txt -f --auto-skip-seconds 1 --date-format "%A, %d %B, %Y" --seconds-per-day 1 --file-idle-time 0 --key --bloom-intensity 0.35 --title "Shop Application Development" \
 
 #--logo ./app/src/notcordsmall.png \ TODO: Fix this to have the shop logo
 

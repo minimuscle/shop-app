58f1e86baa7400ccc09f6a82026362d4b242a466
minimuscle <joshthiele@live.com.au>
2022-07-07 14:45:53 +1000

    fixed course.

---
+++ shop/gource.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gource gource.txt -f --auto-skip-seconds 1 --date-format "%A, %d %B, %Y" --file-idle-time 0 --key --bloom-intensity 0.35 --title "Shop Application Development" \
+
+#--logo ./app/src/notcordsmall.png \ TODO: Fix this to have the shop logo
+
+#--user-image-dir avatars/ \ TODO: Add avatars

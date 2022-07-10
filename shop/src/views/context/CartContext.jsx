fc853f5d36210a7870adbd69a6546c14b2d1f28e
minimuscle <joshthiele@live.com.au>
2022-07-10 18:06:19 +1000

    added hook. cart persistant now

--- shop/src/views/context/CartContext.jsx
+++ shop/src/views/context/CartContext.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import { useLocalStorage } from '../hooks/useLocalStorage'
 
 const CartContext = createContext()
 
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([])
+  const [localCart, setLocalCart] = useLocalStorage('cart', cart)
+
+  useEffect(() => {
+    if (localCart) setCart(localCart)
+  }, [])
 
   const addItem = (product) => {
     setCart((prevState) => [...prevState, product])
+    setLocalCart((prevState) => [...prevState, product])
   }
 
   return (

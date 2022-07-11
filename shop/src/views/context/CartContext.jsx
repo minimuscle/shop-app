756c6628abd6c59feba691c5886119ba1f9b66b1
minimuscle <joshthiele@live.com.au>
2022-07-11 16:45:58 +1000

    cart combined duplicates

--- shop/src/views/context/CartContext.jsx
+++ shop/src/views/context/CartContext.jsx
@@ -13,8 +13,32 @@ export function CartProvider({ children }) {
   }, [])
 
   const addItem = (product) => {
+    //Check if product exists first
+    let exists = false
+    cart.map((i) => {
+      if (i.product_id == product.product_id) {
+        console.log(
+          `found i: ${i.product_id}, and product: ${product.product_id}`
+        )
+        exists = true
+        return i
+      }
+    })
+
+    console.log(`Exists: ${exists}`)
+
+    if (exists) {
+      console.log('doing this one')
+      product.qty = product.qty + 1
+    } else {
+      console.log('nope, this one now')
+      product.qty = 1
+    }
+
     setCart((prevState) => [...prevState, product])
     setLocalCart((prevState) => [...prevState, product])
+
+    console.log(product)
   }
 
   const removeItem = (product) => {
--- shop/src/views/pages/Cart.jsx
+++ shop/src/views/pages/Cart.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react'
 import CartContext from '../context/CartContext'
 import { Container, Card } from '@mui/material'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
 function Cart() {
   const { cart, removeItem } = useContext(CartContext)
   const [totalPrice, setTotalPrice] = useState(0)
+  const [cartList, setCartList] = useState(cart)
 
   function addingPrice() {
     setTotalPrice(
@@ -12,6 +20,15 @@ function Cart() {
       cart.reduce((prev, element) => prev + Number(element.price), 0)
     )
   }
+
+  useEffect(() => {
+    let uniqueObjArray = [
+      ...new Map(cart.map((item) => [item['product_id'], item])).values(),
+    ]
+    setCartList(uniqueObjArray)
+
+    console.log('uniqueObjArray', uniqueObjArray)
+  }, [cart])
 
   useEffect(() => {
     addingPrice()
@@ -23,20 +40,54 @@ function Cart() {
       <span>Total Product Added - {cart.length}</span>
       <br />
       <span>Total - $ {totalPrice}</span>
-      <div className="productContainer">
-        {cart.map((product, key) => {
-          console.log(product.name)
-          return (
-            <Card
-              key={key}
-              className="card"
-              onClick={() => removeItem(product)}
-            >
-              {product.name}
-            </Card>
-          )
-        })}
-      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" colSpan={3}>
+                Details
+              </TableCell>
+              <TableCell align="right">Price</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Qty</TableCell>
+              <TableCell align="right">Unit</TableCell>
+              <TableCell align="right">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {cartList.map((product, key) => (
+              <TableRow key={key}>
+                <TableCell>{product.name}</TableCell>
+                <TableCell align="right">{product.qty}</TableCell>
+                <TableCell align="right">{}</TableCell>
+                <TableCell align="right">
+                  ${product.price * product.qty}
+                </TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell rowSpan={3} />
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell align="right">Subtotal Text</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Tax</TableCell>
+              <TableCell align="right">
+                {/*`${(TAX_RATE * 100).toFixed(
+                0
+              )} %`*/}
+              </TableCell>
+              <TableCell align="right">Invoice Taxes</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell align="right">Invvoice Total</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   )
 }
